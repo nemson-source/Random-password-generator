96882ae75a022a3e2378c8997d30265b4058492c
niko <80647004+nemson-source@users.noreply.github.com>
2023-03-21 20:18:09 +0100

    Add files via upload

---
+++ generator.js
@@ -0,0 +1,61 @@
+const crypto = require('crypto');
+const fs = require('fs')
+const exec = require('child_process').exec;
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+console.log('Enter the length of the password (only numbers):');
+readline.question('', (num) => {
+    console.log('Do you want to include special characters? (y/n)');
+    readline.question('', (specialChars) => {
+        console.log('Do you want to include numbers? (y/n)');
+        readline.question('', (numbers) => {
+            console.log('Enter the usage for this password:');
+            readline.question('', (usage) => {
+                password(num, specialChars, numbers, usage);
+                readline.close();
+            });
+        });
+    });
+});
+
+const password = (num, specialChars, numbers, usage) => {
+    function generateRandomPassword(length, useSpecialChars, useNumbers) {
+        let charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        if (useSpecialChars === 'y') {
+            charset += '!@#$%^&*()_+~`|}{[]:;?><,./-=';
+        }
+        if (useNumbers === 'y') {
+            charset += '0123456789';
+        }
+        let password = '';
+        for (let i = 0; i < length; i++) {
+            const index = crypto.randomInt(0, charset.length);
+            password += charset[index];
+        }
+        return password;
+    }
+
+    // Example usage: generate a random password with length 12
+    const password = generateRandomPassword(num, specialChars, numbers);
+    console.log(password);
+    console.log('You can now copy your password. It is also saved in the file named password.txt');
+    fs.appendFile('password.txt', `usage: ${usage} password: ${password} made on: ${new Date()}\n\n`, (err) => {
+        if (err) throw err;
+    });
+    setTimeout(() => {
+        console.log('Opening password.txt')
+        exec('start password.txt', (err, stdout, stderr) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            console.log(stdout);
+        });
+    }, 3000)
+    setTimeout(() => {
+        process.exit()
+    }, 10000)
+}
